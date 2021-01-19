2b0a8eeec31249be28953496efc31d46c2bac50e
satya9500 <satya.prakash9500@gmail.com>
2021-01-19 17:00:28 +0530

    todos formatted to pleasant strings

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_pleasant_string
+    is_completed = completed ? "[X]" : "[ ]"
+    "#{id}. #{is_completed} #{todo_text} | #{due_date.to_s(:long)}"
+  end
 end
