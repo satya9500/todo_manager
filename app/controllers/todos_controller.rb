8a34a2d566edb7f85c575ef4e4637cb2da3f0679
satya9500 <satya.prakash9500@gmail.com>
2021-01-19 18:25:14 +0530

    create todo HTTP method changed to POST

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
   end
