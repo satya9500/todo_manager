f8a17ec1d2f25b8dca1e063be87a81f9622059f7
satya9500 <satya.prakash9500@gmail.com>
2021-01-22 01:29:25 +0530

    checkbox updates to DB done

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -24,6 +24,6 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "Todo status of #{id} is updated to #{completed}"
+    redirect_to todos_path
   end
 end
