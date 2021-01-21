d80ec6ee748fcba096fe6b7dfd17bd1fdfd976f1
satya9500 <satya.prakash9500@gmail.com>
2021-01-21 22:01:11 +0530

    Updated DB migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_134408) do
+ActiveRecord::Schema.define(version: 2021_01_21_160222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,16 @@ ActiveRecord::Schema.define(version: 2021_01_19_134408) do
     t.text "todo_text"
     t.date "due_date"
     t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
   end
 
 end
