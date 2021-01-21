140331bb263f53ff2e649083780d940d89c64fd0
satya9500 <satya.prakash9500@gmail.com>
2021-01-21 20:36:56 +0530

    erb templating completed

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,20 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{is_completed} #{todo_text} | #{due_date.to_s(:long)}"
   end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where(due_date: Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
 end
