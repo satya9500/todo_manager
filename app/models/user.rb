4854f8ab3b2142a91ac3f4be10e825aee04ca25a
satya9500 <satya.prakash9500@gmail.com>
2021-01-24 01:09:28 +0530

    unique email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :first_name, length: {minimum: 2}
   validates :email, presence: true
+  validates_uniqueness_of :email
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_many :todos
 
