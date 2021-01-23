c1e9f214719a320b73cf3626fc58468890d0885b
satya9500 <satya.prakash9500@gmail.com>
2021-01-23 21:52:52 +0530

    user sign in auth done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
+
   resources :todos
+
   resources :users
-  post "/users/login", to: "users#login"
+
+  get "/signin", to: "sessions#new", as: :new_sessions
+  post "/signin", to: "sessions#create", as: :sessions
 end
