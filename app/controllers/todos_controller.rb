ea9eea486f0bf3c617843dc8bf88d97844f2ed65
satya9500 <satya.prakash9500@gmail.com>
2021-01-24 00:48:04 +0530

    redeploy

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,5 @@
 class TodosController < ApplicationController
   def index
-    # render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
     @todos = current_user.todos
     render "index"
   end
